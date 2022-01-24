0e73b2fe619a6f8bcdfbca1ff28e9395247f048e
윤설 <computer98400@gmail.com>
2022-01-24 22:57:46 +0900

    상위컴포넌트 하위컴포넌트간 통신

---
+++ seol/seolreact/src/App.js
@@ -0,0 +1,56 @@
+// import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import Inputboxtest from './component/Inputboxtest';
+
+
+function App() {
+  const [data, setData] = useState({
+    name: "",
+    age: "",
+    grade: "",
+  });
+
+  const [checkfunc, setCheckfunc] = useState("");
+  const [checkfunc2, setCheckfunc2] = useState("");
+  const [checkfunc3, setCheckfunc3] = useState("");
+  //컴포넌트가 처음 불러와졌을 경우
+  //useEffect(() => { console.log(data) }, [data]);
+  const bottomup = (e) => {
+    setCheckfunc(e.target.value);
+  }
+  
+  const bottomup2 = (e) => {
+    setCheckfunc2(e.target.value);
+  }
+  const bottomup3 = (e) => {
+    setCheckfunc3(e.target.value);
+  }
+  //빈상자 && 보낼 함수.
+  //내가 어떤 값을 받았을 때 지금 컴포넌트의 어떤 값을 받은 값으로 바꾸고싶다.  
+  //자바스크립트는 변수에 함수를 담을 수 있다.
+  const inputTest = (testvalue) => {
+    console.log("check input", testvalue);    
+  }
+
+  const testfunction4 = (test) => {
+    console.log("testfunction4가 실행됫습니다",test);
+  }
+  //상위(큰 컴포넌트)컴포넌트에서 하위(작은 컴포넌트)컴포넌트로 데이터를 전달하는 방법.
+  //props
+  
+  //
+
+  //하위컴포넌트에서 상위컴포넌트로 데이터를 전달하는 방법.
+
+  console.log(checkfunc,checkfunc2,checkfunc3 );
+  return (
+    <div className="App">
+      {/* 인풋 박스에서 값을 받아와야돼  하위컴포넌트에서 상위컴포넌트로 값을 전달하는 방법. */}
+      <Inputboxtest datas={data} testfunction={bottomup} testfunction2={bottomup2} testfunction3={bottomup3} testfunction4={ testfunction4 } inputcheck={ inputTest}/>
+      <button>확인하기.</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ seol/seolreact/src/component/Inputboxtest.js
@@ -0,0 +1,34 @@
+import { useState} from 'react';
+
+export default function Inputboxtest(props) {
+    
+    const [input1, setInput1] = useState("");
+    //지역변수
+    //채킹이라는 함수안에 담는다.
+    //onchange == 값이 변할때마다 동작하는 이벤트
+
+
+    // 지금현재상태인경우에는 input1에 갱신시켜주는것.
+    const checking = (e) => {
+        //setInput1의 경우는 현재 컴포넌트의 state를 변경한것.
+        //setInput1(e.target.value);
+        //상위컴포넌트에 있는 inputcheck를 실행시킨것.
+        //props.inputcheck(e.target.value);
+        props.testfunction4(e.target.value);
+    }
+    console.log(input1);
+    
+
+
+    return (
+        <>
+            <input type="text" onChange={checking}></input>
+            <input type="text" onChange={props.testfunction2}></input>
+            <input type="text" onChange={props.testfunction3}></input>
+        </>
+
+
+    )
+
+
+}
