597350b43b50dbef6ec61b84c3afbfba9f09ec8c
younseol <computer98400@gmail.com>
2022-02-11 23:13:38 +0900

    css 공부내용

--- seol/seolreact/src/reducer/reduce.js
+++ seol/seolreact/src/reducer/reduce.js
@@ -3,9 +3,9 @@ const ADDTOKEN = 'ADDTOKEN';        //이름 정하고
 const DELETETOKEN = 'DELETETOKEN';
 
 //액션 생성자
-export const ADDTK = (diff) => ({ type: ADDTK, diff: diff });    //순수함수 정의 => 그냥 기능. ADDTOKEN 동작을 정의할수잇음. 근데 그게 매번 정의해서 쓸순없으니까 편의성
+export const ADDTK = (diff) => ({ type: ADDTOKEN, diff: diff });    //순수함수 정의 => 그냥 기능. ADDTOKEN 동작을 정의할수잇음. 근데 그게 매번 정의해서 쓸순없으니까 편의성
 
-export const DELETETK = () => ({ type: DELETETK });
+export const DELETETK = () => ({ type: DELETETOKEN });
 
 
 //store 부분이 됨.
