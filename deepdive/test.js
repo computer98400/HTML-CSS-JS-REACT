30a3285443118c0b4c59822e97c6a2e4b33c84c7
computer98400 <computer98400@gmail.com>
2022-02-20 22:45:53 +0900

    [9장] 암묵적 타입변환, 명시적 타입변환

---
+++ deepdive/test.js
@@ -0,0 +1,13 @@
+var test = {
+    property : '1',
+    property2 : '2',
+    property3 : '3',
+}
+
+test['property4'] = 'kyj'
+test.property4 = 'kyj'
+var test2 = 0;
+
+console.log(test3);
+
+var test3 = 0;
