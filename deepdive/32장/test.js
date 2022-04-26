ed582c16cf021db8355ffdd5f863a40fa9e0b452
younseol <computer98400@gmail.com>
2022-04-27 00:21:58 +0900

    [JS study] 32장 비동기 동기 공부중

---
+++ deepdive/32장/test.js
@@ -0,0 +1,28 @@
+
+// 동기 처리의 예시
+function sleep(func, delay) {
+    const delayUntil = Date.now() + delay;
+
+
+    while(Date.now() < delayUntil);
+
+    func();
+
+}
+
+function foo(){
+    console.log('foo');
+}
+function bar(){
+    console.log('bar');
+}
+
+console.log("start");
+// sleep(foo, 3*1000);
+
+// bar();
+
+
+// 비동기 처리의 예시
+setTimeout(foo, 3 * 1000);
+bar();
