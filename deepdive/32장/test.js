9c256357345b44ea3cbc1546c8eeb851c0a001da
younseol <computer98400@gmail.com>
2022-04-27 20:27:03 +0900

    [JS study] 32장 동기식 비동기식

--- deepdive/32장/test.js
+++ deepdive/32장/test.js
@@ -26,3 +26,16 @@ console.log("start");
 // 비동기 처리의 예시
 setTimeout(foo, 3 * 1000);
 bar();
+
+
+
+function foo() {
+    console.log('foo');
+}
+
+function bar() {
+    console.log('bar');
+}
+
+setTimeout(foo, 0);
+bar();
