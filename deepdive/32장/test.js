0cd68598149c5d80dd38950f58bbdd6923fc086a
younseol <computer98400@gmail.com>
2022-05-07 23:43:38 +0900

    [JS study] 33장 이벤트 공부중

--- deepdive/32장/test.js
+++ deepdive/32장/test.js
@@ -1,31 +1,31 @@
 
-// 동기 처리의 예시
-function sleep(func, delay) {
-    const delayUntil = Date.now() + delay;
+// // 동기 처리의 예시(자바스크립트 엔진만을 사용해서 구현한 예제)
+// function sleep(func, delay) {
 
+//     //시간을 밀리초 단위로 판단하는 Date함수를 사용했다.(따라서 number값이 될 수 있음.)
+//     const delayUntil = Date.now() + delay;
+//     while(Date.now() < delayUntil);
 
-    while(Date.now() < delayUntil);
+//     func();
+// }
 
-    func();
+// function foo(){
+//     console.log('foo');
+// }
+// function bar(){
+//     console.log('bar');
+// }
 
-}
+// console.log("start");
 
-function foo(){
-    console.log('foo');
-}
-function bar(){
-    console.log('bar');
-}
-
-console.log("start");
-// sleep(foo, 3*1000);
+// sleep(foo, 3 * 1000);
 
 // bar();
 
 
-// 비동기 처리의 예시
-setTimeout(foo, 3 * 1000);
-bar();
+// // // 비동기 처리의 예시
+// setTimeout(foo, 3 * 1000);
+// bar();
 
 
 
@@ -36,6 +36,6 @@ function foo() {
 function bar() {
     console.log('bar');
 }
-
+//setTimeout의 경우엔  기본적인 차이(4ms) 를 가지고있음.
 setTimeout(foo, 0);
 bar();
